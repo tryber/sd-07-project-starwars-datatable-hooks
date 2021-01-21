20f4ed88641b90898a554b96f14dec45cd29c79b
Lucas <lafdesouza2002@outlook.com>
2021-01-21 16:59:04 -0300

    Commit inicial

---
+++ src/Context/Provider.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import { PropTypes } from 'prop-types';
+import Context from './Context';
+
+const Provider = ({ children }) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+      const json = await response.json();
+      setData(json.results);
+    };
+
+    getData();
+  }, []);
+
+  console.log(children);
+  return (
+    <Context.Provider
+      value={ {
+        data,
+      } }
+    >
+      {children}
+    </Context.Provider>
+  );
+};
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Provider;
---
+++ src/components/Table.js
@@ -0,0 +1,63 @@
+import React, { useState, useContext, useEffect } from 'react';
+import Context from '../Context/Context';
+
+const Table = () => {
+  const { data } = useContext(Context);
+  const [planets, setPlanets] = useState([]);
+  const headers = ['climate', 'created', 'diameter',
+    'edited', 'films', 'gravity',
+    'name', 'orbitalPeriod', 'population',
+    'rotationPeriod', 'surfaceWater', 'terrain', 'url'];
+
+  useEffect(() => {
+    setPlanets(data);
+  }, [data, planets]);
+
+  return (
+    <div>
+      <table>
+        <th>
+          {headers.map((header) => <td key={ header.id }>{header.toUpperCase()}</td>)}
+        </th>
+      </table>
+      {planets.map(({ climate,
+        created,
+        diameter,
+        edited,
+        films,
+        gravity,
+        name,
+        orbitalPeriod,
+        population,
+        rotationPeriod,
+        surfaceWater,
+        terrain,
+        url,
+      }) => (
+        <tr key={ name }>
+          <td>{ climate }</td>
+          <td>{ created }</td>
+          <td>{ diameter }</td>
+          <td>{ edited }</td>
+          <td>
+            {films.map((filme) => (
+              <p key={ filme }>{ filme }</p>
+            ))}
+          </td>
+          <td>{ gravity }</td>
+          <td>{ name }</td>
+          <td>{ orbitalPeriod }</td>
+          <td>{ population }</td>
+          <td>{ rotationPeriod }</td>
+          <td>{ surfaceWater }</td>
+          <td>{ terrain }</td>
+          <td>{ url }</td>
+        </tr>
+      ))}
+    </div>
+  );
+};
+
+export default Table;
+
+// Object.keys()
