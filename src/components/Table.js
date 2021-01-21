a395c27d04b7a1f557d49c7ac2567fd15ee69617
Pedro Marques Fr <pedromarques1011@gmail.com>
2021-01-21 16:12:14 -0300

    corrigido tabela errada

--- src/components/Table.js
+++ src/components/Table.js
@@ -8,13 +8,13 @@ export default function Table({ data }) {
         <tr>
           <th>name</th>
           <th>rotation_period</th>
+          <th>orbital_period</th>
           <th>diameter</th>
           <th>climate</th>
           <th>gravity</th>
           <th>terrain</th>
           <th>surface_water</th>
           <th>population</th>
-          <th>residents</th>
           <th>films</th>
           <th>created</th>
           <th>edited</th>
@@ -26,13 +26,13 @@ export default function Table({ data }) {
           <tr key={ planet.name }>
             <td>{planet.name}</td>
             <td>{planet.rotation_period}</td>
+            <td>{planet.orbital_period}</td>
             <td>{planet.diameter}</td>
             <td>{planet.climate}</td>
             <td>{planet.gravity}</td>
             <td>{planet.terrain}</td>
             <td>{planet.surface_water}</td>
             <td>{planet.population}</td>
-            <td>{planet.residents}</td>
             <td>{planet.films}</td>
             <td>{planet.created}</td>
             <td>{planet.edited}</td>
