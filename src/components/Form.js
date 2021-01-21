e00ecf9c957ee6bc7e6c9bf4b188e9b72e983b3b
Pedro Marques Fr <pedromarques1011@gmail.com>
2021-01-21 17:34:00 -0300

    agr sim

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,12 +2,22 @@ import React, { useContext } from 'react';
 import SWContext from '../context/Context';
 
 export default function Form() {
-  const { filters: { filterByName: { name } }, setName } = useContext(SWContext);
+  const {
+    filters: {
+      filterByName: { name },
+    },
+    setName,
+  } = useContext(SWContext);
   const handleChange = ({ target }) => setName(target.value);
   return (
     <div>
       <form>
-        <input type="text" onChange={ handleChange } value={ name } />
+        <input
+          type="text"
+          onChange={ handleChange }
+          value={ name }
+          data-testid="name-filter"
+        />
       </form>
     </div>
   );
