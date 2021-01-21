d0b238aefe1a3aaa7499244874f454fb0639f5f9
wberilo <wberilo@gmail.com>
2021-01-21 16:54:16 -0300

    req 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import SWContext from './context/SWContext';
+import Table from './components/Table';
 
 function App() {
+  const [apiResponse, setResponse] = useState();
+
+  useEffect(() => {
+    if (apiResponse === undefined) {
+      const fetchFromApi = async () => {
+        try {
+          const data = await fetch('https://swapi-trybe.herokuapp.com/api/planets/')
+            .then((response) => response.json());
+          setResponse(data.results);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      fetchFromApi();
+    }
+  }, [apiResponse]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SWContext.Provider value={ apiResponse }>
+        <Table />
+      </SWContext.Provider>
     </div>
   );
 }
---
+++ src/components/Table.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import SWContext from '../context/SWContext';
+
+function Table() {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>rotation_period</th>
+            <th>orbital_period</th>
+            <th>diameter</th>
+            <th>climate</th>
+            <th>gravity</th>
+            <th>terrain</th>
+            <th>surface_water</th>
+            <th>population</th>
+            <th>residents</th>
+            <th>residents</th>
+            <th>films</th>
+            <th>created</th>
+            <th>edited</th>
+            <th>url</th>
+          </tr>
+        </thead>
+        <tbody>
+          <SWContext.Consumer>
+            {(value) => {
+              if (value) {
+                console.log(value);
+                return value.map((planet) => (
+                  <tr key={ planet.name }>
+                    {Object.keys(planet).map((key) => <td key={ key }>{planet[key]}</td>)}
+                  </tr>
+                ));
+              }
+            }}
+          </SWContext.Consumer>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Table;
