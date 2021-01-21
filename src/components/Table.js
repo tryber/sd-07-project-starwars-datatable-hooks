e455d589f822abf38cef05268d0fe4f6b0b9187c
Lucas <lafdesouza2002@outlook.com>
2021-01-21 17:19:16 -0300

    Requisito 1

--- src/components/Table.js
+++ src/components/Table.js
@@ -16,45 +16,50 @@ const Table = () => {
   return (
     <div>
       <table>
-        <th>
-          {headers.map((header) => <td key={ header.id }>{header.toUpperCase()}</td>)}
-        </th>
-        {planets.map(({ climate,
-          created,
-          diameter,
-          edited,
-          films,
-          gravity,
-          name,
-          orbitalPeriod,
-          population,
-          rotationPeriod,
-          surfaceWater,
-          terrain,
-          url,
-        }) => (
-          <tr key={ name }>
-            <td>{ climate }</td>
-            <td>{ created }</td>
-            <td>{ diameter }</td>
-            <td>{ edited }</td>
-            <td>
-              {films.map((filme) => (
-                <p key={ filme }>{ filme }</p>
-              ))}
-            </td>
-            <td>{ gravity }</td>
-            <td>{ name }</td>
-            <td>{ orbitalPeriod }</td>
-            <td>{ population }</td>
-            <td>{ rotationPeriod }</td>
-            <td>{ surfaceWater }</td>
-            <td>{ terrain }</td>
-            <td>{ url }</td>
+        <thead>
+          <tr>
+            <th>
+              {headers.map((header) => <td key={ header.id }>{header.toUpperCase()}</td>)}
+            </th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {planets.map(({ climate,
+            created,
+            diameter,
+            edited,
+            films,
+            gravity,
+            name,
+            orbitalPeriod,
+            population,
+            rotationPeriod,
+            surfaceWater,
+            terrain,
+            url,
+          }) => (
+            <tr key={ name }>
+              <td>{ climate }</td>
+              <td>{ created }</td>
+              <td>{ diameter }</td>
+              <td>{ edited }</td>
+              <td>
+                {films.map((filme) => (
+                  <p key={ filme }>{ filme }</p>
+                ))}
+              </td>
+              <td>{ gravity }</td>
+              <td>{ name }</td>
+              <td>{ orbitalPeriod }</td>
+              <td>{ population }</td>
+              <td>{ rotationPeriod }</td>
+              <td>{ surfaceWater }</td>
+              <td>{ terrain }</td>
+              <td>{ url }</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
-
     </div>
   );
 };
