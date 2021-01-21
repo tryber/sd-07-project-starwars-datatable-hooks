8812296d31c7c6a6bb1ae4a8c608080ac5911830
rodrigoderkian <derkianpromo@gmail.com>
2021-01-21 19:11:24 -0300

    req:1

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import StarWarsContext from './StarWarsContext';
 
 function Provider({ children }) {
@@ -16,5 +17,9 @@ function Provider({ children }) {
     </StarWarsContext.Provider>
   );
 }
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default Provider;
