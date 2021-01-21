f3502da5491b9b83c03b89cd758f73ad3ddb891b
jvitorof <j.vitoroflores@gmail.com>
2021-01-21 20:02:11 -0300

    CONTEXT: Provider and Consumer init :rocket:

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { StarWarsContext } from './context/StarWarsContext';
+import StarWarsContext from './context/StarWarsContext';
 import { Filters, Table } from './components/index';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <Filters />
-      <Table />
-    </div>
+    <StarWarsContext.Provider value={{}}>
+        <div>
+          <Filters />
+          <Table />
+        </div>
+    </StarWarsContext.Provider>
 
   );
 }
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,43 +1,47 @@
 import React from 'react';
+import StarWarsContext from '../../context/StarWarsContext';
 
 function Table() {
   return (
-    <table>
-      <thead>
-        <tr>
-          <td>HEAD</td>
-          <td>HEAD</td>
-          <td>HEAD</td>
-          <td>HEAD</td>
-          <td>HEAD</td>
-          <td>HEAD</td>
-          <td>HEAD</td>
-          <td>HEAD</td>
-          <td>HEAD</td>
-          <td>HEAD</td>
-          <td>HEAD</td>
-          <td>HEAD</td>
-          <td>HEAD</td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>CELL</td>
-          <td>CELL</td>
-          <td>CELL</td>
-          <td>CELL</td>
-          <td>CELL</td>
-          <td>CELL</td>
-          <td>CELL</td>
-          <td>CELL</td>
-          <td>CELL</td>
-          <td>CELL</td>
-          <td>CELL</td>
-          <td>CELL</td>
-          <td>CELL</td>
-        </tr>
-      </tbody>
-    </table>
+    <StarWarsContext.Consumer>
+      {() => (
+        <table>
+        <thead>
+          <tr>
+            <td>HEAD</td>
+            <td>HEAD</td>
+            <td>HEAD</td>
+            <td>HEAD</td>
+            <td>HEAD</td>
+            <td>HEAD</td>
+            <td>HEAD</td>
+            <td>HEAD</td>
+            <td>HEAD</td>
+            <td>HEAD</td>
+            <td>HEAD</td>
+            <td>HEAD</td>
+            <td>HEAD</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>CELL</td>
+            <td>CELL</td>
+            <td>CELL</td>
+            <td>CELL</td>
+            <td>CELL</td>
+            <td>CELL</td>
+            <td>CELL</td>
+            <td>CELL</td>
+            <td>CELL</td>
+            <td>CELL</td>
+            <td>CELL</td>
+            <td>CELL</td>
+            <td>CELL</td>
+          </tr>
+        </tbody>
+      </table>)}
+    </StarWarsContext.Consumer>
   );
 }
 
