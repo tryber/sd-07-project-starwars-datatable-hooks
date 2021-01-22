463334d0a7e6d81ce94a24ac867a88b6fb2ee2d9
Luise Rios <luisemiranda@gmail.com>
2021-01-22 10:22:16 -0300

    otimizaçao da tabela - requisito 1

--- src/components/Table.js
+++ src/components/Table.js
@@ -14,23 +14,13 @@ function Table() {
         </tr>
       </thead>
       <tbody>
-        {data && data.map((planet) => (
-          <tr key={ planet.name }>
-            <td>{planet.name}</td>
-            <td>{planet.rotation_period}</td>
-            <td>{planet.orbital_period}</td>
-            <td>{planet.diameter}</td>
-            <td>{planet.climate}</td>
-            <td>{planet.gravity}</td>
-            <td>{planet.terrain}</td>
-            <td>{planet.surface_water}</td>
-            <td>{planet.population}</td>
-            <td>{planet.films}</td>
-            <td>{planet.created}</td>
-            <td>{planet.edited}</td>
-            <td>{planet.url}</td>
-          </tr>
-        ))}
+        {
+          data && data.map((planet, index) => (
+            <tr key={ index }>
+              {Object.values(planet).map((value, i) => (<td key={ i }>{value}</td>))}
+            </tr>
+          ))
+        }
       </tbody>
     </table>
   );
