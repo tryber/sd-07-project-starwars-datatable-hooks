27751f8e5436df6f5b1161a6bb268322ced5e6a1
Fernando Antunes <fernando.antunes1@gmail.com>
2021-01-22 15:00:35 -0300

    criado o filtro por coluna

--- src/components/searcher.js
+++ src/components/searcher.js
@@ -3,6 +3,12 @@ import StarWarsContext from '../context/StarWarsContext';
 
 function Searcher() {
   const { handleFilter, filters } = useContext(StarWarsContext);
+  const columns = [
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water'];
 
   return (
     <StarWarsContext.Consumer>
@@ -16,6 +22,14 @@ function Searcher() {
             onChange={ (event) => handleFilter('filterByName', event.target.value) }
             value={ filters.filterByName.name }
           />
+          <label htmlFor="collumns">
+            Selecione uma coluna
+            <select data-testid="column-filter" id="collumns">
+              {columns.map((collum) => (
+                <option key={ collum } value={ collum }>{ collum }</option>
+              ))}
+            </select>
+          </label>
         </div>
       )}
     </StarWarsContext.Consumer>
