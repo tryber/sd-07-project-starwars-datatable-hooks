48dbec40be3b5970e99a22cc7b9bf6b5042b6484
Fernando Antunes <fernando.antunes1@gmail.com>
2021-01-22 17:57:20 -0300

    Requisito 4

--- src/components/searcher.js
+++ src/components/searcher.js
@@ -73,7 +73,7 @@ function Searcher() {
                 ? () => handleFilter('filterByNumericValues') : undefined
             }
           >
-            Filtrar
+            Adicionar Filtro
           </button>
         </div>
       )}
