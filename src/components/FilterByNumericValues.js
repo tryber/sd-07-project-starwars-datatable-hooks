93797636c13d261b4859d92621b68e451e649db7
Vitor Rodrigues <vitor_rc@outlook.com>
2021-01-21 21:08:23 -0300

    Quarto requisito

--- src/components/FilterByNumericValues.js
+++ src/components/FilterByNumericValues.js
@@ -2,22 +2,46 @@ import React, { useContext, useState } from 'react';
 import { StarWarsContext } from '../context/StarWarsContext';
 
 function FilterByNumericValues() {
-  const { filterByNumericValues } = useContext(StarWarsContext);
+  const {
+    filterByNumericValues,
+    filters: { filterByNumericValues: filterNumerics },
+  } = useContext(StarWarsContext);
   const [column, setColumn] = useState('population');
   const [comparison, setComparison] = useState('maior que');
   const [value, setValue] = useState('');
+
+  // const disableOptionSelected = filterNumerics.reduce((acc, filter) => {
+  //     const { comparison } = filter;
+  //     return { ...acc, comparison }
+  // }, {})
+  // console.log(disableOptionSelected)
+
+  const handleClick = () => {
+    filterByNumericValues({ column, comparison, value });
+  };
+
+  const columns = [
+    'population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water',
+  ];
+  const comparisons = ['maior que', 'menor que', 'igual a'];
+
   return (
     <div className="filter-numeric">
+
       <select
         value={ column }
         onChange={ ({ target }) => setColumn(target.value) }
         data-testid="column-filter"
       >
-        <option value="population">population</option>
-        <option value="orbital_period">orbital_period</option>
-        <option value="diameter">diameter</option>
-        <option value="rotation_period">rotation_period</option>
-        <option value="surface_water">surface_water</option>
+        {columns.map((item) => {
+          let isExistColumn = false;
+          filterNumerics.forEach((filter) => {
+            isExistColumn = filter.column === item;
+          });
+          if (isExistColumn) return null;
+
+          return (<option value={ item } key={ item }>{ item }</option>);
+        })}
       </select>
 
       <select
@@ -25,9 +49,15 @@ function FilterByNumericValues() {
         onChange={ ({ target }) => setComparison(target.value) }
         data-testid="comparison-filter"
       >
-        <option value="maior que">maior que</option>
-        <option value="menor que">menor que</option>
-        <option value="igual a">igual a</option>
+        {comparisons.map((operand) => {
+          let isExistOperand = false;
+          filterNumerics.forEach((filter) => {
+            isExistOperand = filter.column === operand;
+          });
+          if (isExistOperand) return null;
+
+          return (<option value={ operand } key={ operand }>{ operand }</option>);
+        })}
       </select>
 
       <input
@@ -40,7 +70,7 @@ function FilterByNumericValues() {
       <button
         type="button"
         data-testid="button-filter"
-        onClick={ () => filterByNumericValues({ column, comparison, value }) }
+        onClick={ handleClick }
       >
         Adicionar Filtro
       </button>
