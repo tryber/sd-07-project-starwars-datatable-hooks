fbd9eea1e9ef8e72a6523aeb03e2935719290279
Felipe Nascimento <flpnascto@gmail.com>
2021-01-22 16:05:21 -0300

    Componente para input de filtros

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Provider from './context/Provider';
 import RequestData from './components/RequestData';
 import TablePlanets from './components/TablePlanets';
+import FiltersHeader from './components/FiltersHeader';
 
 function App() {
   return (
     <Provider>
       <div>
+        <FiltersHeader />
         <RequestData />
         <TablePlanets />
       </div>
---
+++ src/components/FiltersHeader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import NameFilter from './NameFilter';
+
+function FiltersHeader() {
+  return (
+    <NameFilter />
+  );
+}
+
+export default FiltersHeader;
