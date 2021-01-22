9421d1177b4ac8d8a13eefd269f4b105c3bbcad2
samantabelow <samanta.below@gmail.com>
2021-01-22 16:24:57 -0300

    Requisito 2

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,10 +1,29 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 
 function Table() {
   const { apiResults } = useContext(StarWarsContext);
+  const [filterWord, setFilterWord] = useState({
+    filters: {
+      filterByName: { name: '' },
+    },
+  });
+
+  const handleInputChange = ({ target }) => {
+    const word = target.value;
+    setFilterWord({
+      filters: {
+        filterByName: { name: word },
+      },
+    });
+  };
+
   return (
     <div>
+      <input
+        data-testid="name-filter"
+        onChange={ (target) => handleInputChange(target) }
+      />
       <table>
         <thead>
           <tr>
@@ -22,23 +41,25 @@ function Table() {
             <th>Edited</th>
             <th>Url</th>
           </tr>
-          {apiResults.map((item) => (
-            <tr key={ item.name }>
-              <td>{item.name}</td>
-              <td>{item.rotation_period}</td>
-              <td>{item.orbital_period}</td>
-              <td>{item.diameter}</td>
-              <td>{item.climate}</td>
-              <td>{item.gravity}</td>
-              <td>{item.terrain}</td>
-              <td>{item.surface_water}</td>
-              <td>{item.population}</td>
-              <td>{item.films}</td>
-              <td>{item.created}</td>
-              <td>{item.edited}</td>
-              <td>{item.url}</td>
-            </tr>
-          ))}
+          {apiResults
+            .filter((item) => item.name.includes(filterWord.filters.filterByName.name))
+            .map((item) => (
+              <tr key={ item.name }>
+                <td>{item.name}</td>
+                <td>{item.rotation_period}</td>
+                <td>{item.orbital_period}</td>
+                <td>{item.diameter}</td>
+                <td>{item.climate}</td>
+                <td>{item.gravity}</td>
+                <td>{item.terrain}</td>
+                <td>{item.surface_water}</td>
+                <td>{item.population}</td>
+                <td>{item.films}</td>
+                <td>{item.created}</td>
+                <td>{item.edited}</td>
+                <td>{item.url}</td>
+              </tr>
+            ))}
         </thead>
       </table>
     </div>
