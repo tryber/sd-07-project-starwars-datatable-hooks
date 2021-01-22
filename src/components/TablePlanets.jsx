eebd4a9198dbf1733dadd0f3960d1be60e176a0f
Felipe Nascimento <flpnascto@gmail.com>
2021-01-21 21:09:24 -0300

    Componente TablePlanets

---
+++ src/components/TablePlanets.jsx
@@ -0,0 +1,53 @@
+import React, { useContext } from 'react';
+import StarWarsContext from '../context/StarWarsContext';
+
+const TablePlanets = () => {
+  const { data } = useContext(StarWarsContext);
+  if (!data) return null;
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Período de Rotação</th>
+            <th>Período Orbital</th>
+            <th>Diâmetro</th>
+            <th>Clima</th>
+            <th>Gravidade</th>
+            <th>Terreno</th>
+            <th>Surface Water</th>
+            <th>População</th>
+            <th>Filmes</th>
+            <th>Criado</th>
+            <th>Editado</th>
+            <th>URL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((planet) => (
+              <tr key={ planet.name }>
+                <td>{ planet.name }</td>
+                <td>{ planet.rotation_period }</td>
+                <td>{ planet.orbital_period }</td>
+                <td>{ planet.diameter }</td>
+                <td>{ planet.climate }</td>
+                <td>{ planet.gravity }</td>
+                <td>{ planet.terrain }</td>
+                <td>{ planet.surface_water }</td>
+                <td>{ planet.population }</td>
+                <td>{ planet.films }</td>
+                <td>{ planet.created }</td>
+                <td>{ planet.edited }</td>
+                <td>{ planet.url }</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TablePlanets;
