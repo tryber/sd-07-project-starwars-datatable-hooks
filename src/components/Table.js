6eeec8e7d832330ff5b2fcdcf4e4d819f7385ac9
Kramer <kramerscs@gmail.com>
2021-01-23 20:43:24 -0300

    fix: lint

--- src/components/Table.js
+++ src/components/Table.js
@@ -22,21 +22,22 @@ function Table() {
           <td>created</td>
           <td>edited</td>
         </tr>
-        {data.map((planet) => (<tr key={ planet.name }>
-          <td>{planet.name}</td>
-          <td>{planet.rotation_period}</td>
-          <td>{planet.orbital_period}</td>
-          <td>{planet.diameter}</td>
-          <td>{planet.climate}</td>
-          <td>{planet.gravity}</td>
-          <td>{planet.terrain}</td>
-          <td>{planet.surface_water}</td>
-          <td>{planet.population}</td>
-          <td>{planet.residents}</td>
-          <td>{planet.films}</td>
-          <td>{planet.created}</td>
-          <td>{planet.edited}</td>
-        </tr>))}
+        {data.map((planet) => (
+          <tr key={ planet.name }>
+            <td>{planet.name}</td>
+            <td>{planet.rotation_period}</td>
+            <td>{planet.orbital_period}</td>
+            <td>{planet.diameter}</td>
+            <td>{planet.climate}</td>
+            <td>{planet.gravity}</td>
+            <td>{planet.terrain}</td>
+            <td>{planet.surface_water}</td>
+            <td>{planet.population}</td>
+            <td>{planet.residents}</td>
+            <td>{planet.films}</td>
+            <td>{planet.created}</td>
+            <td>{planet.edited}</td>
+          </tr>))}
       </table>
     </div>
   );
