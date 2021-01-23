5c759135cd7ca4c42146c50ca73ebaae2e473365
laurods <itrafor@gmail.com>
2021-01-23 08:06:36 -0200

    Requisito 1 corrigido Lint

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ function App() {
   return (
     <div className="App">
       <StartWarsProvider>
-          <Table />
+        <Table />
       </StartWarsProvider>
-      
     </div>
   );
 }
--- src/context/StarWarsProvider.js
+++ src/context/StarWarsProvider.js
@@ -1,26 +1,28 @@
 import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import StarWarsContext from './StarWarsContext';
 
-function Provider ({ children }) {
-const [data, setData] = useState([]);
-const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
+function Provider({ children }) {
+  const [data, setData] = useState([]);
+  const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
 
-useEffect(() => {
-    async function fetchData(){
-        const { results } = await fetch(url).then((response) => response.json());
-        setData(results);
+  useEffect(() => {
+    async function fetchData() {
+      const { results } = await fetch(url).then((response) => response.json());
+      setData(results);
     }
     fetchData();
-}, []);
+  }, []);
 
-
-return (
-    <StarWarsContext.Provider value={{ data }}>
-        { children }
+  return (
+    <StarWarsContext.Provider value={ { data } }>
+      { children }
     </StarWarsContext.Provider>
-);
-
-
+  );
 }
 
 export default Provider;
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
