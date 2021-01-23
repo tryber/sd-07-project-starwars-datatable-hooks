ebb77e69f43a83422fecbcdc8609b76081229649
Quintiliano Dalete <quintilianodalete@gmail.com>
2021-01-23 13:59:37 -0300

    feat: requirement 3

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -2,7 +2,11 @@ import React, { useContext } from 'react';
 import StarWarsContext from '../../context/StarWarsContext';
 
 export default function Filters() {
-  const { handleChange } = useContext(StarWarsContext); // Defining wich resources from the Provider I`m using in this component
+  const {
+    handleChange,
+    handleNumericFilter,
+    handleFilterButton,
+  } = useContext(StarWarsContext); // Defining wich resources from the Provider I`m using in this component
 
   return (
     <section>
@@ -15,6 +19,51 @@ export default function Filters() {
           onChange={ ({ target }) => handleChange(target.value) }
         />
       </label>
+      <label htmlFor="column">
+        Filter by name
+        <select
+          data-testid="column-filter"
+          onChange={ ({ target }) => handleNumericFilter(target.name, target.value) }
+          name="column"
+          id="column"
+        >
+          <option>population</option>
+          <option>diameter</option>
+          <option>orbital_period</option>
+          <option>rotation_period</option>
+          <option>surface_water</option>
+        </select>
+      </label>
+      <label htmlFor="comparation">
+        Filter by name
+        <select
+          data-testid="comparison-filter"
+          onChange={ ({ target }) => handleNumericFilter(target.name, target.value) }
+          name="comparation"
+          id="comparation"
+        >
+          <option>maior que</option>
+          <option>menor que</option>
+          <option>igual a</option>
+        </select>
+      </label>
+      <label htmlFor="name">
+        Filter by name
+        <input
+          data-testid="value-filter"
+          type="number"
+          id="value"
+          onChange={ ({ target }) => handleNumericFilter(target.name, target.value) }
+          name="value"
+        />
+      </label>
+      <button
+        data-testid="button-filter"
+        type="button"
+        onClick={ handleFilterButton }
+      >
+        Add filter
+      </button>
     </section>
   );
 }
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -10,11 +10,45 @@ function StarWarsProvider({ children }) {
     filterByName: {
       name: '',
     },
+    filterByNumber: [
+      {
+        column: 'diameter',
+        comparation: 'menor que',
+        value: '',
+      },
+    ],
   });
 
   const fetchPlanets = async () => {
     const planets = await getPlanetsApi();
     setData(planets);
+  };
+
+  const handleNumericFilter = (name, value) => {
+    setFilter({
+      ...filter,
+      filterByNumber: [
+        {
+          ...filter.filterByNumber[0],
+          [name]: value,
+        },
+      ],
+    });
+  };
+
+  const handleFilterButton = () => {
+    const { column, comparation, value } = filter.filterByNumber[0];
+
+    if (comparation === 'maior que') {
+      const filteredPlanets = data.filter((planet) => +value < +planet[column]);
+      setData(filteredPlanets);
+    } else if (comparation === 'menor que') {
+      const filteredPlanets = data.filter((planet) => +value > +planet[column]);
+      setData(filteredPlanets);
+    } else {
+      const filteredPlanets = data.filter((planet) => +value === +planet[column]);
+      setData(filteredPlanets);
+    }
   };
 
   useEffect(() => {
@@ -44,6 +78,8 @@ function StarWarsProvider({ children }) {
   const context = { // Just the states and functions listed here will be used by the children
     data, // Iformation used in the component Table
     handleChange, // Function used in the component Filters
+    handleNumericFilter, // Function used in the component Filters
+    handleFilterButton,
   };
 
   return (
