e240db5bff1ce595f4c6b5de7a5c7d156627ecf2
Breenolf <blf@ic.ufal.br>
2021-01-23 18:53:30 -0300

    fix error lint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Header.css';
 
-const Header = () => {
+function Header() {
   return (
     <header>
       <h1>Star Wars Planetarium</h1>
     </header>
   );
-};
+}
 
 export default Header;
