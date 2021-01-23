1fd38ade6fef35f2f49cece832f10c3aca52c1a2
Iury Nathan <iurynathan@gmail.com>
2021-01-23 14:11:56 -0300

    requisito 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import StarWarsContext from './context/StarWarsContext';
+import TablePlanet from './components/Table';
+import FilterByName from './components/FilterByName';
 
 function App() {
+  const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
+  const [starWars, setStarWars] = useState([]);
+  const [filters, setFilters] = useState({
+    filterByName: {
+      name: '' },
+  });
+
+  const initialState = {
+    starWars,
+    setStarWars,
+    filters,
+    setFilters,
+  };
+
+  useEffect(() => {
+    async function data() {
+      const { results } = await fetch(url)
+        .then((response) => response.json());
+      setStarWars(results);
+      console.log(results);
+    }
+    data();
+  }, [url]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <StarWarsContext.Provider value={ initialState }>
+        <FilterByName />
+        <TablePlanet />
+      </StarWarsContext.Provider>
     </div>
   );
 }
