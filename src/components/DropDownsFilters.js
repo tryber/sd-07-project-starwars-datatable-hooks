4f317b59991c447b2e0206f8df60fe0e435177dc
MoisesSantana <moisaant@gmail.com>
2021-01-23 16:52:54 -0300

    DropDownsFilters Component pronta - sem eventos e funções

--- src/components/DropDownsFilters.js
+++ src/components/DropDownsFilters.js
@@ -2,7 +2,45 @@ import React from 'react';
 
 function DropDownsFilters() {
   return (
-    <p>DropDownsFilters</p>
+    <div>
+      <label htmlFor="filterColumn">
+        <select
+          id="filterColumn"
+          data-testid="column-filter"
+        >
+          <option>population</option>
+          <option>orbital_period</option>
+          <option>diameter</option>
+          <option>rotation_period</option>
+          <option>surface_water</option>
+        </select>
+      </label>
+
+      <label htmlFor="arithmeticLogic">
+        <select
+          id="arithmeticLogic"
+          data-testid="comparison-filter"
+        >
+          <option>maior que</option>
+          <option>menor que</option>
+          <option>igual a</option>
+        </select>
+      </label>
+      <label htmlFor="expectedNumber">
+        <input
+          type="number"
+          name="expectedNumber"
+          id="expectedNumber"
+          data-testid="value-filter"
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="button-filter"
+      >
+        FILTER
+      </button>
+    </div>
   );
 }
 
