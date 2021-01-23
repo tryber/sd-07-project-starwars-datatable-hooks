6acbd63829101a65606bca3b845a5d33137ad6cd
MoisesSantana <moisaant@gmail.com>
2021-01-23 14:59:34 -0300

    requisito 1

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,21 +5,14 @@ function Table() {
   const contextValue = useContext(StarWarsContext);
   const [headers, setHeaders] = useState([]);
 
-  const renderizeHeaders = () => {
-    const { data: { results } } = contextValue;
-    if (!results) return 'loading...';
-    const getHeaders = Object.keys(results[1])
-      .filter((header) => header !== 'residents');
-    setHeaders(getHeaders);
-  };
-
-  const renderizeLines = () => {
-    const { data: { results } } = contextValue;
-
-  };
-
   useEffect(() => {
-    renderizeHeaders();
+    const { data: { results } } = contextValue;
+    if (results) {
+      const getHeaders = Object.keys(results[1])
+        .filter((header) => header !== 'residents');
+      setHeaders(getHeaders);
+    }
+    // renderizeHeaders();
   }, [contextValue]);
 
   return (
@@ -28,6 +21,41 @@ function Table() {
       <tr>
         {headers.map((header) => <th key={ header }>{ header }</th>)}
       </tr>
+      {contextValue.data.results && contextValue.data.results.map((object, index) => {
+        const {
+          name,
+          rotation_period: rotationPeriod,
+          orbital_period: orbitalPeriod,
+          diameter,
+          climate,
+          gravity,
+          terrain,
+          surface_water: surfaceWater,
+          population,
+          films,
+          created,
+          edited,
+          url,
+        } = object;
+
+        return (
+          <tr key={ `line${index}` }>
+            <td key={ `name${index}` }>{ name }</td>
+            <td key={ `rotation_period${index}` }>{ rotationPeriod }</td>
+            <td key={ `orbital_period${index}` }>{ orbitalPeriod }</td>
+            <td key={ `diameter${index}` }>{ diameter }</td>
+            <td key={ `climate${index}` }>{ climate }</td>
+            <td key={ `gravity${index}` }>{ gravity }</td>
+            <td key={ `tarrain${index}` }>{ terrain }</td>
+            <td key={ `surface_water${index}` }>{ surfaceWater }</td>
+            <td key={ `population${index}` }>{ population }</td>
+            <td key={ `films${index}` }>{ films }</td>
+            <td key={ `created${index}` }>{ created }</td>
+            <td key={ `edited${index}` }>{ edited }</td>
+            <td key={ `url${index}` }>{ url }</td>
+          </tr>
+        );
+      })}
     </table>
   );
 }
