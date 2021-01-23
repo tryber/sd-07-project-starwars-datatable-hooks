57bb6723279329af575421ff6b7e4c573bd42845
laurods <itrafor@gmail.com>
2021-01-23 11:00:44 -0200

    Requisito 2 corrigido Lint

--- src/context/StarWarsProvider.js
+++ src/context/StarWarsProvider.js
@@ -4,18 +4,36 @@ import StarWarsContext from './StarWarsContext';
 
 function Provider({ children }) {
   const [data, setData] = useState([]);
+  const [filters, setFilters] = useState([]);
+
   const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
 
   useEffect(() => {
     async function fetchData() {
       const { results } = await fetch(url).then((response) => response.json());
       setData(results);
+      setFilters(results);
     }
     fetchData();
   }, []);
 
+  const filteredPlanetsByName = (value) => {
+    let planetFiltred = null;
+
+    if (value !== '') {
+      planetFiltred = data.filter((planet) => planet.name.includes(value));
+      setFilters(planetFiltred);
+    } else {
+      setFilters(data);
+    }
+  };
+
+  const handleName = ({ target }) => {
+    const { value } = target;
+    filteredPlanetsByName(value);
+  };
   return (
-    <StarWarsContext.Provider value={ { data } }>
+    <StarWarsContext.Provider value={ { filters, handleName } }>
       { children }
     </StarWarsContext.Provider>
   );
