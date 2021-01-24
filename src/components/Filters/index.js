a1f7db4d360b7115e21edab302f8d097e06aca5a
CarolSi <a.carolg1@gmail.com>
2021-01-24 19:58:39 -0300

    corrige bug do filtro

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -1,25 +1,27 @@
 import React, { useContext } from 'react';
 import StarWarsContext from '../../context/StarWarsContext';
+import './Filters.css';
 
 function Filters() {
   const { filters: { filterByNumericValues:
      objectFilter },
   deleteFilter } = useContext(StarWarsContext);
   return (
-    <div>
-      {objectFilter.map((item) => (
-        <p data-testid="filter" key={ item.column }>
-          { item.column }
-          { item.comparison }
-          { item.value }
-          <button
-            type="button"
-            onClick={ () => deleteFilter(item.column, item.comparision) }
-          >
-            X
-          </button>
-        </p>
-      ))}
+    <div className="filters-container">
+      <div>
+        {objectFilter.map((item) => (
+          <p data-testid="filter" key={ item.column }>
+            {`${item.column} ${item.comparison} ${item.value}`}
+            <button
+              className="delete-button"
+              type="button"
+              onClick={ () => deleteFilter(item.column, item.comparison) }
+            >
+              X
+            </button>
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
