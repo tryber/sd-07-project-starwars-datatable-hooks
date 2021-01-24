cdadb8c208f92e0bf8b96fee11deb0b3d97ae014
murilopereira006 <muriloedu006@gmail.com>
2021-01-24 03:44:15 -0300

    bora fechar esse req 3!

--- src/componentes/SearchBar.jsx
+++ src/componentes/SearchBar.jsx
@@ -10,6 +10,23 @@ function SearchBar() {
         data-testid="name-filter"
         onChange={ (event) => filterNameInput(event) }
       />
+      <select data-testid='column-filter'>
+        <option>population</option>
+        <option>orbital_period</option>
+        <option>diameter</option>
+        <option>rotation_period</option>
+        <option>surface_water</option>
+      </select>
+      <select data-testid='comparison-filter'>
+        <option>maior que</option>
+        <option>menor que</option>
+        <option>igual a</option>
+      </select>
+      <input
+        type="number"
+        data-testid='value-filter'
+      />
+      <button type="submit" data-testid='button-filter'>Filtrar</button>
     </form>
   );
 }
