aec892c430ecc94fb47717cf2ce6dd91885b5765
BrunoNogueiraBruno <nugobruno@gmail.com>
2021-01-27 13:47:21 -0300

    2nd Requirement IV

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,6 +3,40 @@ import MyContext from '../context/MyContext';
 import tableCells from './tableCells';
 
 class Table extends React.Component {
+  constructor() {
+    super();
+    this.getCells = this.getCells.bind(this);
+    this.getRows = this.getRows.bind(this);
+  }
+
+  getCells(cells) {
+    const listOfCells = [];
+    cells.forEach((cell, index) => {
+      listOfCells.push(
+        <td key={ `cell-${index}` }>
+          {cell}
+        </td>,
+      );
+    });
+    return listOfCells;
+  }
+
+  getRows(value) {
+    const planets = Object
+      .entries(value)
+      .map((each) => each[1]);
+    const listOfRows = [];
+
+    planets.forEach((planet, index) => {
+      listOfRows.push(
+        <tr key={ `row-${index}` }>
+          {this.getCells(Object.values(planet))}
+        </tr>,
+      );
+    });
+    return listOfRows;
+  }
+
   render() {
     return (
       <MyContext.Consumer>
@@ -10,14 +44,14 @@ class Table extends React.Component {
           const { data } = value;
           return (
             <table className="table">
-              <tbody>
-                {tableCells.getRows(data)}
-              </tbody>
               <thead>
                 <tr>
                   {tableCells.getHeaders(data[0])}
                 </tr>
               </thead>
+              <tbody>
+                {this.getRows(data)}
+              </tbody>
             </table>
           );
         }}
