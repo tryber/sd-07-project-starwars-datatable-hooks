fd7dcac3c3c244c2345985afd903c20ccef71aaa
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2021-01-29 09:57:16 -0300

    requisito 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,8 +2,11 @@ import React, { useContext } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 
 export default function Table() {
-  const { filters, setFilters } = useContext(StarWarsContext);
+  const { filters, setFilters, setValues, inputValues, setInputValues, handleClick } = useContext(StarWarsContext);
   const { filterByName } = filters;
+  const { filterByNumericValues } = filters;
+  const columns = ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
+  const comparators = ['maior que', 'menor que', 'igual a'];
 
   return (
     <div>
@@ -17,6 +20,48 @@ export default function Table() {
           onChange={ (e) => setFilters({ ...filters, filterByName: { name: e.target.value } }) }
         />
       </label>
+      <div>
+
+        <select
+          data-testid="column-filter"
+          id="column"
+          value={ filterByNumericValues.column }
+          onChange={ (e) => setValues(e.target.id, e.target.value) }
+        >
+          {columns.map((option) => (
+            <option key={ option } value={ option }>{option}</option>
+          ))}
+        </select>
+
+        <select
+          data-testid="comparison-filter"
+          id="comparison"
+          value={ filterByNumericValues.comparison }
+          onChange={ (e) => setValues(e.target.id, e.target.value) }
+        >
+          {comparators.map((option) => (
+            <option key={ option } value={ option }>{option}</option>
+          ))}
+        </select>
+
+        <input
+          type="number"
+          step={ 1 }
+          id="value"
+          data-testid="value-filter"
+          value={ filterByNumericValues.value }
+          onChange={ (e) => setValues(e.target.id, e.target.value) }
+        />
+
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ () => handleClick() }
+        >
+          Filtrar
+        </button>
+
+      </div>
     </div>
   );
 }
--- src/context/StarWarsProvider.jsx
+++ src/context/StarWarsProvider.jsx
@@ -4,28 +4,84 @@ import StarWarsContext from './StarWarsContext';
 import StarWarsAPI from '../services/StarWarsAPI';
 
 const StarWarsProvider = ({ children }) => {
+  const [backupPlanets, setBackupPlanets] = useState([]);
   const [data, setData] = useState([]);
   const [filters, setFilters] = useState(
     {
       filterByName: { name: '' },
-      filterByNumericValues: [],
+      filterByNumericValues: [
+        {
+          column: '',
+          comparison: '',
+          value: '',
+        },
+      ],
+    },
+  );
+  const [inputValues, setInputValues] = useState(
+    { column: '',
+      comparison: '',
+      value: 0,
     },
   );
 
   const fetchData = async () => {
     const getPlanets = await StarWarsAPI();
     setData(getPlanets);
+    setBackupPlanets(getPlanets);
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
+  const setValues = (name, value) => {
+    console.log(name, value);
+    setInputValues({ ...inputValues, [name]: value });
+  };
+
+  const imprimir = () => {
+    console.log(filters);
+  };
+
+  const handleClick = () => {
+    const { filterByNumericValues } = filters;
+    // setFilters(
+    //   { ...filters, filterByNumericValues: [...filterByNumericValues, inputValues] },
+    // );
+
+    let filterComparison = data;
+
+    const { column, comparison, value } = inputValues;
+    switch (comparison) {
+    case 'maior que':
+      filterComparison = [...data.filter((planet) => +(planet[column]) > +value)];
+      setData([...filterComparison]);
+      break;
+    case 'menor que':
+      filterComparison = [...data.filter((planet) => +(planet[column]) < +(value))];
+      setData([...filterComparison]);
+      break;
+    case 'igual a':
+      filterComparison = [...data.filter(
+        (planet) => +(planet[column]) === +(value),
+      )];
+      setData([...filterComparison]);
+      break;
+    default:
+      return filterComparison;
+    }
+  };
+
   const context = {
     fetchData,
     data,
     filters,
     setFilters,
+    setInputValues,
+    inputValues,
+    handleClick,
+    setValues,
   };
   return (
     <StarWarsContext.Provider value={ context }>
