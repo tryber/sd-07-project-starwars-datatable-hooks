b5d18b447a1fef69be754fb8905d22c1ed3a7e9f
Quintiliano Dalete <quintilianodalete@gmail.com>
2021-02-04 19:54:53 -0300

    adding bibliographic reference

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+// Referencias bibliograficas:
+// Para o desenvolvimento deste projeto usei como referencia as seguintes fontes:
+// Repositorio do colega Carlos Souza: https://github.com/tryber/sd-07-project-starwars-datatable-hooks/tree/carlos-souza-react-context-hooks-starwars-datatable-filters;
+// Repositorio do colega Tiago Esdras:https://github.com/tryber/sd-07-project-starwars-datatable-hooks/tree/tiago-esdras-starwars-datatable.
+// StackOverFlow
+// Fontes diversas do Google.
+
 import React from 'react';
 import Provider from './context/Provider';
 import Table from './components/Table';
