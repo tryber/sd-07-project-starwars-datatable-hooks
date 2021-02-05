7bf7c66562241a35be8ba08072df6b476496e987
Nicholas Torres J. Vasconcelos <nicolastorresjatoba@gmail.com>
2021-02-05 15:12:59 -0300

    Erros lint resolvidos

--- src/components/StarWarsPage.js
+++ src/components/StarWarsPage.js
@@ -4,11 +4,11 @@ import StarWarsTable from './StarWarsTable';
 import FormStarWars from './FormStarWars';
 
 export default function StarWarsPage() {
-  const { fetchPlanets, isFetching, planetsStarWars } = useContext(StarWarsContext);
+  const { fetchPlanets, isFetching } = useContext(StarWarsContext);
 
   useEffect(() => {
     fetchPlanets();
-  }, [planetsStarWars]);
+  }, [fetchPlanets]);
 
   return (
     <div>
