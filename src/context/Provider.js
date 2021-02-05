4917f9f666039aac89733a84d226a26bbc0fa536
rodrigoderkian <derkianpromo@gmail.com>
2021-02-04 21:42:23 -0300

    creditos e explicaçoes

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -20,6 +20,16 @@ function Provider({ children }) {
     getFetch();
   }, []);
 
+  // Para a construção desta lógica, consultei alguns repositórios dos colegas. Não sei se enquadra em plágio, e infelizmente, não sei a quem exatamente deveria creditar.
+  // Gostaria de explicar o funcionamento, para demonstrar que não possuo má fé, e que aprendi o conteúdo.
+  // A função a seguir 'allFilters' lida com os filtros de valores ('maior que', 'menor que' e 'igual a').
+  // Primeiramente, ele esvazia quaisquer informação anterior (setData([])), e compara os valores de 'comparison', que é obtido atraves do onChange no <select> dos filtros (vide linha 57 do componente Filters).
+  // No primeiro if, se 'comparison' tiver o valor de 'maior que', a função modificará o estado Data (responsável pelas informações renderizadas na tela), filtrando os itens que possuam os valores na 'column' (este valor é obtido através do <select> dos filtros (vide linha 33 do componente Filters)) maiores que os encontrados em value (este obtido no input dos filtros (vide linha 76 do componente Filters)).
+  // Não sendo o caso, a função fara a conferencia novamente, e se 'comparison' receber o valor de 'menor que', a função modificará o estado Data, filtrando os itens que possuam os valores na 'column' dos filtros (vide linha 33 do componente Filters)) menores que os encontrados em 'value' (este obtido no input dos filtros (vide linha 76 do componente Filters)).
+  // Não sendo nenhum dos casos anteriores, nos resta conferir se o valor de 'column' é igual ao do 'value', e Data receberá os valores que se enquadram nessa situação.
+  // Após cada filtragem, queremos que esse tipo de filtro não seja mais possível (é o caso do requisito 4, 'não utilize filtros repetidos), então, mudaremos o estado das opções (option), utilizando o setOptions e filtrando os valores diferentes do utilizado na filtragem anterior. Assim, toda vez que for feito um filtro, essa opção deixa de ser exibida para ser selecionada, evitando que o usuário filtre duas vezes com o mesmo critério.
+  // Peço desculpas se feri alguma regra da Trybe, e me coloco a disposição para explicar o código e suas funcionalidades.
+
   const allFilters = () => {
     setData([]);
     if (comparison === 'maior que') {
