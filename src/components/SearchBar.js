3bf534dd0f57d7d8a0438a7b7f3803ed8ccb8550
Bruna Fonsêca <1998bfonseca@gmail.com>
2021-02-11 10:59:43 -0300

    lint issues fixed

--- src/components/SearchBar.js
+++
@@ -1,9 +0,0 @@
-import React, { useContext, useState } from 'react';
-import StarWarsContext from '../context/StarWarsContext';
-
-function SearchBar() {
-  const { planets } = useContext(StarWarsContext);
-
- 
-
-
