cfcdd5b7c2751bc6301557d398a9d56ea3a06d1d
josielvs <josielvs@gmail.com>
2021-02-11 15:01:19 -0300

    Alteração de dados de teste GitHub - Lint

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Provider from './context/Provider';
-import FilterByName from './components/FilterByName';
 import FilterByNumeric from './components/FilterByNumeric';
 import TablePlanets from './components/TablePlanets';
+import FilterByName from './components/FilterByName';
 import './App.css';
 
 function App() {
