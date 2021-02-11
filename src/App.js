c7aa1c40b42a0ef47b98a8e687b3f25dd1f5e814
josielvs <josielvs@gmail.com>
2021-02-11 15:07:53 -0300

    Alteração de dados de teste GitHub

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Provider>
       <div>
-        Planets - Star Wars
+        <h5>Planets - Star Wars</h5>
         <FilterByName />
         <FilterByNumeric />
         <TablePlanets />
