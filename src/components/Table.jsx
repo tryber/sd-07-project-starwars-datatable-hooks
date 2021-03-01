73f1434bcb88121c0e574d8bd56dfc15898b49c4
Arthur de Sa <arthurdesa94@gmail.com>
2021-03-01 17:08:28 -0300

    me ajuda ai prettier

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -22,22 +22,22 @@ function Table() {
           {pageLoading
             ? 'Loading'
             : data.map((planet) => (
-                <tr key={ planet.name }>
-                  <td data-testid="planet-name">{ planet.name }</td>
-                  <td>{ planet.rotation_period }</td>
-                  <td>{ planet.orbital_period }</td>
-                  <td>{ planet.diameter }</td>
-                  <td>{ planet.climate }</td>
-                  <td>{ planet.gravity }</td>
-                  <td>{ planet.terrain }</td>
-                  <td>{ planet.surface_water }</td>
-                  <td>{ planet.population }</td>
-                  <td>{ planet.films }</td>
-                  <td>{ planet.created }</td>
-                  <td>{ planet.edited }</td>
-                  <td>{ planet.url }</td>
-                </tr>
-              ))}
+              <tr key={ planet.name }>
+                <td data-testid="planet-name">{ planet.name }</td>
+                <td>{ planet.rotation_period }</td>
+                <td>{ planet.orbital_period }</td>
+                <td>{ planet.diameter }</td>
+                <td>{ planet.climate }</td>
+                <td>{ planet.gravity }</td>
+                <td>{ planet.terrain }</td>
+                <td>{ planet.surface_water }</td>
+                <td>{ planet.population }</td>
+                <td>{ planet.films }</td>
+                <td>{ planet.created }</td>
+                <td>{ planet.edited }</td>
+                <td>{ planet.url }</td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
