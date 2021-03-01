df9aef9639368b1a0194b011ea530473ed133f0e
Arthur de Sa <arthurdesa94@gmail.com>
2021-03-01 16:45:50 -0300

    lint1.1

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -16,28 +16,28 @@ function Table() {
         <tr>
           {pageLoading
             ? null
-            : headers.map((tHead) => <th key={tHead}>{tHead}</th>)}
+            : headers.map((tHead) => <th key={ tHead }>{ tHead }</th>)}
         </tr>
         <tbody>
           {pageLoading
             ? 'Loading'
             : data.map((planet) => (
-                <tr key={planet.name}>
-                  <td data-testid="planet-name">{planet.name}</td>
-                  <td>{planet.rotation_period}</td>
-                  <td>{planet.orbital_period}</td>
-                  <td>{planet.diameter}</td>
-                  <td>{planet.climate}</td>
-                  <td>{planet.gravity}</td>
-                  <td>{planet.terrain}</td>
-                  <td>{planet.surface_water}</td>
-                  <td>{planet.population}</td>
-                  <td>{planet.films}</td>
-                  <td>{planet.created}</td>
-                  <td>{planet.edited}</td>
-                  <td>{planet.url}</td>
+                <tr key={ planet.name }>
+                  <td data-testid="planet-name">{ planet.name }</td>
+                  <td>{ planet.rotation_period }</td>
+                  <td>{ planet.orbital_period }</td>
+                  <td>{ planet.diameter }</td>
+                  <td>{ planet.climate }</td>
+                  <td>{ planet.gravity }</td>
+                  <td>{ planet.terrain }</td>
+                  <td>{ planet.surface_water }</td>
+                  <td>{ planet.population }</td>
+                  <td>{ planet.films }</td>
+                  <td>{ planet.created }</td>
+                  <td>{ planet.edited }</td>
+                  <td>{ planet.url }</td>
                 </tr>
-              ))}
+            ))}
         </tbody>
       </table>
     </div>
