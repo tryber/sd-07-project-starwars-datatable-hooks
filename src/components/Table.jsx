a53cd355c465816d009b34a582f19556b733ed76
Arthur de Sa <arthurdesa94@gmail.com>
2021-03-01 16:40:46 -0300

    lint1

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,7 +2,9 @@ import React, { useContext } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 
 function Table() {
-  const { data, pageLoading, setpageLoading, headers} = useContext(StarWarsContext);
+  const { data, pageLoading, setpageLoading, headers } = useContext(
+    StarWarsContext,
+  );
   const zero = 0;
   if (data.length && headers.length > zero) {
     setpageLoading(false);
@@ -12,28 +14,30 @@ function Table() {
       <h1>Tabela</h1>
       <table>
         <tr>
-          {pageLoading ? null : headers.map((tHead) => (
-            <th key={ tHead }>{tHead}</th>
-          ))}
+          {pageLoading
+            ? null
+            : headers.map((tHead) => <th key={tHead}>{tHead}</th>)}
         </tr>
         <tbody>
-          {pageLoading ? 'Loading' : data.map((planet) => (
-            <tr key={ planet.name }>
-              <td data-testid="planet-name">{planet.name}</td>
-              <td>{planet.rotation_period}</td>
-              <td>{planet.orbital_period}</td>
-              <td>{planet.diameter}</td>
-              <td>{planet.climate}</td>
-              <td>{planet.gravity}</td>
-              <td>{planet.terrain}</td>
-              <td>{planet.surface_water}</td>
-              <td>{planet.population}</td>
-              <td>{planet.films}</td>
-              <td>{planet.created}</td>
-              <td>{planet.edited}</td>
-              <td>{planet.url}</td>
-            </tr>
-          ))}
+          {pageLoading
+            ? 'Loading'
+            : data.map((planet) => (
+                <tr key={planet.name}>
+                  <td data-testid="planet-name">{planet.name}</td>
+                  <td>{planet.rotation_period}</td>
+                  <td>{planet.orbital_period}</td>
+                  <td>{planet.diameter}</td>
+                  <td>{planet.climate}</td>
+                  <td>{planet.gravity}</td>
+                  <td>{planet.terrain}</td>
+                  <td>{planet.surface_water}</td>
+                  <td>{planet.population}</td>
+                  <td>{planet.films}</td>
+                  <td>{planet.created}</td>
+                  <td>{planet.edited}</td>
+                  <td>{planet.url}</td>
+                </tr>
+              ))}
         </tbody>
       </table>
     </div>
