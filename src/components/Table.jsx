0cd3924d5457666ba8956129c5a57ab61b766010
Arthur de Sa <arthurdesa94@gmail.com>
2021-03-02 12:39:13 -0300

    po prettier ai nao part2

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -21,29 +21,29 @@ function Table() {
             : headers.map((tHead) => <th key={tHead}>{tHead}</th>)}
         </tr>
         <tbody>
-          {pageLoading
-            ? 'Loading'
-            : data
-                .filter((planet) =>
-                  planet.name.toLowerCase().includes(name.toLowerCase())
-                )
-                .map((planet) => (
-                  <tr key={ planet.name}>
-                    <td data-testid="planet-name">{ planet.name}</td>
-                    <td>{ planet.rotation_period }</td>
-                    <td>{ planet.orbital_period }</td>
-                    <td>{ planet.diameter }</td>
-                    <td>{ planet.climate }</td>
-                    <td>{ planet.gravity }</td>
-                    <td>{ planet.terrain }</td>
-                    <td>{ planet.surface_water }</td>
-                    <td>{ planet.population }</td>
-                    <td>{ planet.films }</td>
-                    <td>{ planet.created }</td>
-                    <td>{ planet.edited }</td>
-                    <td>{ planet.url }</td>
-                  </tr>
-                ))}
+           { pageLoading
+           ? 'Loading'
+           : data
+            .filter((planet) =>
+            planet.name.toLowerCase()
+            .includes(name.toLowerCase()))
+            .map((planet) => (
+              <tr key={ planet.name}>
+                <td data-testid="planet-name">{ planet.name}</td>
+                <td>{ planet.rotation_period }</td>
+                <td>{ planet.orbital_period }</td>
+                <td>{ planet.diameter }</td>
+                <td>{ planet.climate }</td>
+                <td>{ planet.gravity }</td>
+                <td>{ planet.terrain }</td>
+                <td>{ planet.surface_water }</td>
+                <td>{ planet.population }</td>
+                <td>{ planet.films }</td>
+                <td>{ planet.created }</td>
+                <td>{ planet.edited }</td>
+                <td>{ planet.url }</td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
