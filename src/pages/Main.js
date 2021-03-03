8b64cd72986afb42d04a8802b10fcc039b532237
Fabio Camargos <fabiocmmcm@gmail.com>
2021-03-03 20:10:03 -0300

    trying to fix error from useEfects

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -10,44 +10,44 @@ function Main() {
   useEffect(() => {
     setPlanetsData(data);
   }, [data]);
-
-  const handleDataFilters = () => {
-    const { filterByName: { name }, filterByNumericValues } = filters;
-    const ZERO = 0;
-
-    if (name) {
-      const newData = data.filter((planet) => (
-        planet.name.toLowerCase().includes(name.toLowerCase())
-      ));
-      setplanetsData(newData);
-    }
-
-    if (filterByNumericValues.length > ZERO) {
-      const lastFilter = filterByNumericValues.length - 1;
-      const { column, comparison, value } = filterByNumericValues[lastFilter];
-
-      const operator = {
-        'maior que': (a, b) => Number(a) > Number(b),
-        'menor que': (a, b) => Number(a) < Number(b),
-        'igual a': (a, b) => Number(a) === Number(b),
-      };
-
-      const newData = data.filter((planet) => (
-        (operator[comparison](planet[column], value) && planet[column] !== 'unknown')
-          ? planet
-          : undefined
-      ));
-
-      setplanetsData(newData);
-    }
-
-    if (!name && !(filterByNumericValues.length > ZERO)) {
-      setplanetsData(data);
-    }
-  };
-
+  
   useEffect(() => {
-    handleDataFilters();
+    const handleDataFilters = () => {
+      const { filterByName: { name }, filterByNumericValues } = filters;
+      const ZERO = 0;
+
+      if (name) {
+        const newData = data.filter((planet) => (
+          planet.name.toLowerCase().includes(name.toLowerCase())
+        ));
+        setplanetsData(newData);
+      }
+
+      if (filterByNumericValues.length > ZERO) {
+        const lastFilter = filterByNumericValues.length - 1;
+        const { column, comparison, value } = filterByNumericValues[lastFilter];
+
+        const operator = {
+          'maior que': (a, b) => Number(a) > Number(b),
+          'menor que': (a, b) => Number(a) < Number(b),
+          'igual a': (a, b) => Number(a) === Number(b),
+        };
+
+        const newData = data.filter((planet) => (
+          (operator[comparison](planet[column], value) && planet[column] !== 'unknown')
+            ? planet
+            : undefined
+        ));
+
+        setplanetsData(newData);
+      }
+
+      if (!name && !(filterByNumericValues.length > ZERO)) {
+        setplanetsData(data);
+      }
+    };
+
+      handleDataFilters();
   }, [filters]);
 
   return (
