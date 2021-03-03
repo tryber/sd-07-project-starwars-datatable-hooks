1639f1f7adcc170eb5fd6f80bb6f48b63ad7db49
Arthur de Sa <arthurdesa94@gmail.com>
2021-03-03 16:34:20 -0300

    fixing lint

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -18,34 +18,38 @@ function Filter() {
       value: numberFilter,
     };
     setFilterNumber(filtering);
-  }
+  };
 
   return (
     <div>
-      <input
-        onChange={ handleChange }
-        type="text"
-        data-testid="name-filter"
-      />
-            <select data-testid="column-filter" id="column-filter">
-        <option value="population" key="population">population</option>
-        <option value="orbital_period" key="orbital_period">orbital_period</option>
-        <option value="diameter" key="diameter">diameter</option>
-        <option value="rotation_period" key="rotation_period">rotation_period</option>
-        <option value="surface_water" key="surface_water">surface_water</option>
+      <input onChange={handleChange} type="text" data-testid="name-filter" />
+      <select data-testid="column-filter" id="column-filter">
+        <option value="population" key="population">
+          population
+        </option>
+        <option value="orbital_period" key="orbital_period">
+          orbital_period
+        </option>
+        <option value="diameter" key="diameter">
+          diameter
+        </option>
+        <option value="rotation_period" key="rotation_period">
+          rotation_period
+        </option>
+        <option value="surface_water" key="surface_water">
+          surface_water
+        </option>
       </select>
       <select data-testid="comparison-filter" id="comparison-filter">
-        <option hidden value="">Selecione</option>
+        <option hidden value="">
+          Selecione
+        </option>
         <option value="maior que">maior que</option>
         <option value="menor que">menor que</option>
         <option value="igual a">igual a</option>
       </select>
       <input data-testid="value-filter" type="number" id="number-filter" />
-      <button
-        data-testid="button-filter"
-        type="button"
-        onClick={ handleClick }
-      >
+      <button data-testid="button-filter" type="button" onClick={handleClick}>
         Filtrar
       </button>
     </div>
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,7 +2,9 @@ import React, { useContext } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 
 function Table() {
-  const { data, pageLoading, setpageLoading, headers, filters } = useContext(StarWarsContext);
+  const { data, pageLoading, setpageLoading, headers, filters } = useContext(
+    StarWarsContext,
+  );
   const { filterByName, numericValuesFiltered } = filters;
   const { name } = filterByName;
   const { column, comparison, value } = numericValuesFiltered[0];
@@ -12,57 +14,61 @@ function Table() {
   }
 
   const filterOptions = (planets) => {
-    if(comparison === '') {
+    if (comparison === '') {
       return true;
     }
-    if(comparison === 'maior que') {
-      if(Number(planets[column]) > Number(value)) {
+    if (comparison === 'maior que') {
+      if (Number(planets[column]) > Number(value)) {
         return true;
       }
     }
-    if(comparison === 'menor que') {
-      if(Number(planets[column]) < Number(value)) {
+    if (comparison === 'menor que') {
+      if (Number(planets[column]) < Number(value)) {
         return true;
       }
     }
-    if(comparison === 'igual a') {
-      if(Number(planets[column]) === Number(value)) {
+    if (comparison === 'igual a') {
+      if (Number(planets[column]) === Number(value)) {
         return true;
       }
     }
     return false;
-  }
+  };
 
   return (
     <div>
       <h1>Tabela</h1>
       <table>
         <tr>
-          {pageLoading ? null : headers.map((tHead) => (
-            <th key={ tHead }>{tHead}</th>
-          ))}
+          {pageLoading
+            ? null
+            : headers.map((tHead) => <th key={tHead}>{tHead}</th>)}
         </tr>
         <tbody>
-          {pageLoading ? 'Loading' : data.filter((planets) => filterOptions(planets))
-            .filter((planet) => planet.name.toLowerCase().includes(name.toLowerCase()))
-              .map((planet) => (
-                <tr key={ planet.name }>
-                  <td data-testid="planet-name">{planet.name}</td>
-                  <td>{planet.rotation_period}</td>
-                  <td>{planet.orbital_period}</td>
-                  <td>{planet.diameter}</td>
-                  <td>{planet.climate}</td>
-                  <td>{planet.gravity}</td>
-                  <td>{planet.terrain}</td>
-                  <td>{planet.surface_water}</td>
-                  <td>{planet.population}</td>
-                  <td>{planet.films}</td>
-                  <td>{planet.created}</td>
-                  <td>{planet.edited}</td>
-                  <td>{planet.url}</td>
-                </tr>
-              ))
-          }
+          {pageLoading
+            ? 'Loading'
+            : data
+                .filter((planets) => filterOptions(planets))
+                .filter((planet) =>
+                  planet.name.toLowerCase().includes(name.toLowerCase()),
+                )
+                .map((planet) => (
+                  <tr key={planet.name}>
+                    <td data-testid="planet-name">{planet.name}</td>
+                    <td>{planet.rotation_period}</td>
+                    <td>{planet.orbital_period}</td>
+                    <td>{planet.diameter}</td>
+                    <td>{planet.climate}</td>
+                    <td>{planet.gravity}</td>
+                    <td>{planet.terrain}</td>
+                    <td>{planet.surface_water}</td>
+                    <td>{planet.population}</td>
+                    <td>{planet.films}</td>
+                    <td>{planet.created}</td>
+                    <td>{planet.edited}</td>
+                    <td>{planet.url}</td>
+                  </tr>
+                ))}
         </tbody>
       </table>
     </div>
