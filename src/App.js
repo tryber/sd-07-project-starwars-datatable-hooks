f725ed5bcf9270fc2e2143eb337cd50e663acbea
Fabio Camargos <fabiocmmcm@gmail.com>
2021-03-03 23:16:50 -0300

    cc =/

--- src/App.js
+++ src/App.js
@@ -20,4 +20,3 @@ function App() {
 }
 
 export default App;
-//desito desse projeto sem cabeca, so quero fazer meu backend
--- src/contexts/PlanetContext.js
+++ src/contexts/PlanetContext.js
@@ -1,4 +1,3 @@
-
 import React, { createContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
