22bb2d6b52a4e425243216a1f8a3d9d80a39d9b2
dhiegodias <67385608+dhiegodias@users.noreply.github.com>
2021-03-04 18:31:53 -0300

    Delete src directory

--- src/services/planetsAPI.js
+++
@@ -1,8 +0,0 @@
-const URL = 'https://swapi-trybe.herokuapp.com/api/planets/';
-
-const getPlanetsAPI = async () => {
-  const { results } = await fetch(URL).then((response) => response.json());
-  return results;
-};
-
-export default getPlanetsAPI;
