c06ccd7f7ad359fc672c79dff580e6f099a97b03
Fabio Camargos <fabiocmmcm@gmail.com>
2021-03-04 13:12:23 -0300

    Filtering, Table and .gitHub fixed

--- src/components/PlanetTable.js
+++ src/components/PlanetTable.js
@@ -1,9 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { PlanetContext } from '../contexts/PlanetContext';
 import PlanetRow from './PlanetRow';
 
 const PlanetsTable = () => {
   const { planets } = useContext(PlanetContext);
+  const [nameFilter, setNameFilter] = useState('');
+  const [filtersState, setFiltersState] = useState({
+    filters: {
+      filterByName: {
+        name: '',
+      },
+    },
+  });
+
+  useEffect(() => {
+    setFiltersState({
+      ...filtersState,
+      filters: { filterByName: { name: nameFilter } },
+    });
+  }, [nameFilter]);
+
   const headers = [
     'name',
     'rotation period',
@@ -19,17 +35,33 @@ const PlanetsTable = () => {
     'edited',
     'url',
   ];
+
   return planets ? (
-    <table className="table">
-      <thead>
-        <tr>
-          {headers.map((header) => (<th key={ header }>{header}</th>))}
-        </tr>
-      </thead>
-      <tbody>
-        {planets.map((planet) => <PlanetRow key={ planet.name } planet={ planet } />)}
-      </tbody>
-    </table>
+  <div>
+      <input
+        type="text"
+        data-testid="name-filter"
+        placeholder="planet name filter"
+        value={ nameFilter }
+        onChange={ (e) => setNameFilter(e.target.value) }
+      />
+      <table className="table">
+        <thead>
+          <tr>
+            {headers.map((header) => (<th key={ header }>{header}</th>))}
+          </tr>
+        </thead>
+        <tbody>
+          {planets.map((planet) => {
+            const lowerCasedPlanetName = planet.name.toLowerCase();
+            const lowerCasedNameFilter = filtersState
+              .filters.filterByName.name.toLowerCase();
+            return (lowerCasedPlanetName.match(lowerCasedNameFilter))
+              && <PlanetRow key={ planet.name } planet={ planet } />;
+          })}
+        </tbody>
+      </table>
+    </div>
   ) : (
     <p>Loading...</p>
   );
