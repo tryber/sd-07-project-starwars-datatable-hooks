ff1d8d99aab3b30b11e855107d1e5136a9da4db9
Arthur de Sa <arthurdesa94@gmail.com>
2021-03-04 19:49:07 -0300

    agora vai os 90tao nos 45 do segundo tempo simboraaa

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -12,43 +12,57 @@ function Filter() {
     const columnSelect = document.getElementById('column-filter').value;
     const comparisonSelect = document.getElementById('comparison-filter').value;
     const numberFilter = document.getElementById('number-filter').value;
-    const filtering = {
+    const filterBy = {
       column: columnSelect,
       comparison: comparisonSelect,
       value: numberFilter,
     };
-    setFilterNumber(filtering);
+    setFilterNumber(filterBy);
   };
+
+  const resetFilter = () => {
+    const filterBy = {
+      column: '',
+      comparison: '',
+      value: '',
+    };
+    setFilterNumber(filterBy);
+  }
+
 
   return (
     <div>
       <input onChange={ handleChange } type="text" data-testid="name-filter" />
-      <select data-testid="column-filter" id="column-filter">
-        <option value="population" key="population">
-          population
-        </option>
-        <option value="orbital_period" key="orbital_period">
-          orbital_period
-        </option>
-        <option value="diameter" key="diameter">
-          diameter
-        </option>
-        <option value="rotation_period" key="rotation_period">
-          rotation_period
-        </option>
-        <option value="surface_water" key="surface_water">
-          surface_water
-        </option>
-      </select>
-      <select data-testid="comparison-filter" id="comparison-filter">
-        <option hidden value="">
-          Selecione
-        </option>
-        <option value="maior que">maior que</option>
-        <option value="menor que">menor que</option>
-        <option value="igual a">igual a</option>
-      </select>
-      <input data-testid="value-filter" type="number" id="number-filter" />
+      <div data-testid="filter">
+        <select data-testid="column-filter" id="column-filter">
+          <option value="population" key="population">population</option>
+          <option value="orbital_period" key="orbital_period">orbital_period</option>
+          <option value="diameter" key="diameter">diameter</option>
+          <option value="rotation_period" key="rotation_period">rotation_period</option>
+          <option value="surface_water" key="surface_water">surface_water</option>
+        </select>
+        <select data-testid="comparison-filter" id="comparison-filter">
+          <option hidden value="">Selecione</option>
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+        <input data-testid="value-filter" type="number" id="number-filter" />
+        <button
+          data-testid="filter"
+          type="button"
+          onClick={ resetFilter }
+        >
+          X
+        </button>
+      </div>
+      <button
+          data-testid="filter"
+          type="button"
+          onClick={ resetFilter }
+        >
+          X
+        </button>
       <button data-testid="button-filter" type="button" onClick={ handleClick }>
         Filtrar
       </button>
