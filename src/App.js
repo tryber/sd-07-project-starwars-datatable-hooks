0e5fc1f98562420692bba5b7eb1a47fdb317551e
Fabio Camargos <fabiocmmcm@gmail.com>
2021-03-04 13:05:23 -0300

    Forget to add App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import PlanetsTable from './components/PlanetTable';
 import PlanetContextProvider from './contexts/PlanetContext';
 
 function App() {
   return (
     <div>
       <PlanetContextProvider>
+        <PlanetsTable />
       </PlanetContextProvider>
     </div>
   );
