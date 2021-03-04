b60d0824c67ad7112fe2db110f9d3e37faf63608
nyxnil <nsmnilsonn@gmail.com>
2021-03-04 18:00:16 -0300

    subindo projeto 0.7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,159 +1,62 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 
-// Utilização do Context - modelo de estudo e crédito total { Bruno Sordi }
-// Foram feitas observações também nos códigos do { Pedro Marques }
-//
-// Dados da API passado pelo Context
 function Table() {
-  const {
-    data,
-    filters,
-    setFilters,
-    filterHandler,
-    setFilterHandler,
-  } = useContext(StarWarsContext);
+  const { data, setData, filters, setFilters } = useContext(StarWarsContext);
 
-  // Filtro por nome, a cada novo filter, pega o array original e filtra por nome
+  useEffect(() => {
+    async function fetchData() {
+      const { results } = await fetch(
+        'https://swapi-trybe.herokuapp.com/api/planets/',
+      ).then((response) => response.json());
+      results.map((planet) => delete planet.residents);
+      setData(results);
+    }
+    fetchData();
+  });
+
   function handleChangeName({ target }) {
-    const prevFilters = filters;
-    setFilters({
-      ...prevFilters,
-      filterByName: { name: target.value },
-    });
+    setFilters({ filters: { filterByName: { name: target.value } } });
   }
 
-  function handleChangeSelected({ target }) {
-    const prevFilters = filterHandler;
-    const { name, value } = target;
-    setFilterHandler({
-      ...prevFilters,
-      [name]: value,
-    });
-  }
-  // Adicionar filtro numerico
-  function addFilter() {
-    const prevFilters = filters;
-    setFilters({
-      ...prevFilters,
-      filterByNumericValues: [...prevFilters.filterByNumericValues, filterHandler],
-    });
-  }
+  function renderTableBody(planet) {
+    const tableReturn = Object.values(planet).map((element, index) => (
+      <td key={ index }>{ element }</td>
+    ));
+    const { name } = filters.filters.filterByName;
 
-  function renderOptions() {
-    const { filterByNumericValues } = filters;
-    const createdFilters = [];
-    filterByNumericValues.map((filter) => createdFilters.push(filter.column));
-    const arrayComparison = [
-      'population',
-      'orbital_period',
-      'diameter',
-      'rotation_period',
-      'surface_water',
-    ];
-    const arrayRendered = arrayComparison.filter((comparison) => (
-      createdFilters.every((filter) => filter !== comparison)
-    ));
-
-    return (
-      <label htmlFor="column">
-        <select
-          name="column"
-          data-testid="column-filter"
-          value={ filters.filterByNumericValues.column }
-          onChange={ handleChangeSelected }
-        >
-          {arrayRendered.map((comparison, index) => (
-            <option key={ index } value={ comparison }>
-              { comparison }
-            </option>
-          ))}
-        </select>
-      </label>
-    );
-  }
-  // deletando a coluna de filtro // refatorar para ser excluido somente 1 valor
-  function deleteFilter(column) {
-    const { filterByNumericValues } = filters;
-    const delFilters = filterByNumericValues.filter((filter) => (
-      filter.column !== column
-    ));
-    setFilters({
-      ...filters,
-      filterByNumericValues: delFilters,
-    });
-  }
-  // novo render apartir do filte populacional
-  function renderFilters() {
-    const { filterByNumericValues } = filters;
-    const renderedFilters = filterByNumericValues.map((filter, index) => (
-      <div data-testid="filter" key={ index }>
-        { `${filter.column} ${filter.comparison} ${filter.value}  `}
-        <button type="button" onClick={ () => deleteFilter(filter.column) }>x</button>
-      </div>
-    ));
-    return renderedFilters;
+    const lower = tableReturn[0].props.children.toLocaleLowerCase();
+    if (lower.includes(name.toLocaleLowerCase()) || name === '') {
+      return tableReturn;
+    }
   }
 
   return (
     <div>
-      <div>
-        <label htmlFor="filter-text">
-          Filter By Name:
-          <input
-            type="text"
-            name="filter-text"
-            data-testid="name-filter"
-            value={ filters.filterByName.name }
-            onChange={ handleChangeName }
-          />
-        </label>
-        {renderOptions()}
-        <label htmlFor="comparison">
-          <select
-            name="comparison"
-            data-testid="comparison-filter"
-            value={ filters.filterByNumericValues.comparison }
-            onChange={ handleChangeSelected }
-          >
-            <option value="maior que">maior que</option>
-            <option value="igual a">igual a</option>
-            <option value="menor que">menor que</option>
-          </select>
-        </label>
-        <label htmlFor="value">
-          <input
-            data-testid="value-filter"
-            name="value"
-            value={ filters.filterByNumericValues.value }
-            onChange={ handleChangeSelected }
-          />
-        </label>
-        <button type="button" data-testid="button-filter" onClick={ addFilter }>
-          Add Filters
-        </button>
-        <div>
-          { renderFilters() }
-        </div>
-      </div>
-      {!data.length ? (
+      <label htmlFor="filter-text">
+        Filtro:
+        <input
+          type="text"
+          name="filter-text"
+          data-testid="name-filter"
+          value={ filters.filters.filterByName.name }
+          onChange={ handleChangeName }
+        />
+      </label>
+      {data === undefined ? (
         <div>Loading...</div>
       ) : (
         <table>
           <thead>
             <tr>
-              { Object.keys(data[0]).map((element, index) => (
+              {Object.keys(data[0]).map((element, index) => (
                 <th key={ index }>{ element }</th>
               ))}
             </tr>
           </thead>
           <tbody>
-            {data.map((item, keys) => (
-              <tr key={ keys }>
-                { Object.values(item).map((element, index) => (
-                  <td key={ index }>{ element }</td>
-                ))}
-              </tr>
+            { data.map((item, index) => (
+              <tr key={ index }>{ renderTableBody(item) }</tr>
             ))}
           </tbody>
         </table>
