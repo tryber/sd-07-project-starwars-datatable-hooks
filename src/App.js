33e0623f6f613ebac3ebcee807b651f8c75c1c21
Gustavo Saideles da Silveira <saideles.silveira@gmail.com>
2021-03-05 12:05:16 -0300

    limpando

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Table from './components/Table';
 import Provider from './context/Provider';
-import StarWarsContext from './context/StarWarsContext';
+
 // Fontes:
 // https://github.com/tryber/sd-07-project-starwars-datatable-hooks/pull/129
 // https://github.com/tryber/sd-07-project-starwars-datatable-hooks/pull/51
