0c7daa4aa3524607a6b31dc57ed6dc6cc8d6fb04
Gustavo Saideles da Silveira <saideles.silveira@gmail.com>
2021-03-05 12:03:59 -0300

    acho que foi

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Header from './components/Header';
+import Table from './components/Table';
+import StarWarsContext from './context/StarWarsContext';
+// Fontes:
+// https://github.com/tryber/sd-07-project-starwars-datatable-hooks/pull/129
+// https://github.com/tryber/sd-07-project-starwars-datatable-hooks/pull/51
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StarWarsContext>
+      <Header />
+      <Table />
+    </StarWarsContext>
+
   );
 }
 
